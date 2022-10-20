2f7f24ad807fd2812ef7e167a761066663d14cab
euimokna <31657666+euimokna@users.noreply.github.com>
2022-10-20 13:44:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      Project = "Coffee-Mug-Cake-2"
+      Project = "Coffee-Mug-Cake"
       Owner   = "jerry & tom"
     }
   }
